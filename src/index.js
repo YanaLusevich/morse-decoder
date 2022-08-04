e4337a487035b725f83225e992ee1a7e75372d9f
YanaLusevich <preety_lynx@mail.ru>
2022-08-05 00:05:31 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let newString = '';
+    let arr = [];
+
+    for (let i = 0; i < expr.length; i++) {
+        newString += expr[i];
+        if(newString.length == 10) {
+            arr.push(newString);
+            newString = '';
+        }
+    //console.log(newString);
+    }
+
+    let newArr =[];
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].replace(/10/gi, '.');
+        arr[i] = arr[i].replace(/11/gi, '-');
+        arr[i] = arr[i].replace(/\*/gi, ' ');
+    newArr.push(arr[i]);
+    }
+
+    for (let i = 0; i < newArr.length; i++) {
+        newArr[i] = newArr[i].replace(/^0+/, '');
+    }
+
+    let result = '';
+
+    for (let a of newArr) {
+        result += MORSE_TABLE[a] ||' ';
+    }
+return result;
 }
 
 module.exports = {
     decode
-}
+};
